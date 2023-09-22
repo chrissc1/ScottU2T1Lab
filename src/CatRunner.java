2a4e5ae8eabd7057cb48ddf305ce75aa53954a2b
BT_2N2_22 <sample@bths.edu>
2023-09-22 14:04:39 -0400

    doing this just incase

---
+++ src/CatRunner.java
@@ -0,0 +1,9 @@
+public class CatRunner {
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Connor", 14, 18.9);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+    }
+}
